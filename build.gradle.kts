9123e6343349a1b2642a88b4527d7dc0bd311772
David Groomes <dwgroomes@gmail.com>
2023-01-21 23:57:53 -0600

    Upgrade root Gradle to 7.5.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,7 +30,7 @@ project("single-file-source") {
 
     java {
         toolchain {
-            languageVersion.set(JavaLanguageVersion.of(15))
+            languageVersion.set(JavaLanguageVersion.of(17))
         }
     }
 
